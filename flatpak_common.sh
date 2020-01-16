8e1f4bd4ef43b135da9f4d5fc6d13933ac7b4c2e
lefterisALEX <lefterisalexakis@gmail.com>
2020-01-16 10:40:06 +0100

    add flatpak apps

---
+++ flatpak_common.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+flatpak install flathub \
+    org.keepassxc.KeePassXC \
+    com.visualstudio.code \
+    com.spotify.Client \
+    com.github.calo001.fondo \
+    com.discordapp.Discord \
+    org.remmina.Remmina \
+    com.slack.Slack -y        
