005d6e1853ffc01dd7a4d826e24a397d1f13d74c
lefterisALEX <lefterisalexakis@gmail.com>
2020-01-16 10:49:32 +0100

    add brave and dropbox

---
+++ brave_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com `lsb_release -sc` main" >> /etc/apt/sources.list.d/brave.list'
+sudo apt update
+sudo apt install brave-browser brave-keyring
---
+++ dropbox_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+echo "To start daemon execute the following command"
+echo "~/.dropbox-dist/dropboxd to start daemon"
